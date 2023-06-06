9f582885663b891cb134089394b67cb633ea243c
DoDongssss <xchitts@gmail.com>
2023-06-06 17:12:16 +0800

    fixed module

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import 'aos/dist/aos.css'; 
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -7,7 +8,6 @@ import App from './App.vue'
 import router from './router'
 
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
 
 const app = createApp(App)
 
